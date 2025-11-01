1dc92198cff87b465f56eeca8d524d8793098991
Julie-Denes <julie.denes29@gmail.com>
2025-11-01 15:39:34 +0100

    ajout de la page supprimer une equipe

--- src/delete/supprimer_equipe.php
+++ src/delete/supprimer_equipe.php
@@ -4,33 +4,31 @@ require __DIR__ . "/../includes/header.php";
 
 use App\PDO\EquipePDO;
 
-// FONCTION DE SUPPRESSION D'UNE ÉQUIPE
+
 function supprimerEquipe(PDO $pdo, int $id): bool
 {
-    $equipe = EquipePDO::getById($pdo, $id); // VÉRIFICATION SI EXISTE
-    if ($equipe)
-    {
+    $equipe = EquipePDO::getById($pdo, $id);
+    if ($equipe) {
         EquipePDO::delete($pdo, $equipe);
+
         return true;
     }
 
     return false;
 }
 
-// Récupérer l'ID depuis l'URL
-$id = $_GET['id'] ?? null;
-if ($id && is_numeric($id))
-    {
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+if ($id !== null && $id !== false) {
     try {
-        if (!supprimerEquipe($pdo, (int)$id))
-        {
-            echo "<p style='color:red'>Équipe introuvable.</p>";
+        if (!supprimerEquipe($pdo, (int)$id)) {
+
         }
     } catch (PDOException $e) {
-        echo "<p style='color:red'>Erreur lors de la suppression : "
-            . htmlspecialchars($e->getMessage()) . "</p>";
+
     }
 }
 
+// Revenir à la page INDEX.PHP
 header("Location: ../../public/index.php");
 exit;
